2474a1567a2c057679f2dd8567161357db37b68b
Stefan1210 <sjvdkort@ziggo.nl>
2026-05-31 21:46:18 +0200

    feat: voeg application interfaces en DTOs toe

---
+++ src/Centaur.Application/DTOs/LoginRequest.cs
@@ -0,0 +1,3 @@
+namespace Centaur.Application.DTOs;
+
+public record LoginRequest(string Email, string Password);
---
+++ src/Centaur.Application/Interfaces/IUserRepository.cs
@@ -0,0 +1,10 @@
+using Centaur.Domain.Entities;
+
+namespace Centaur.Application.Interfaces;
+
+public interface IUserRepository
+{
+    Task<User?> GetByEmailAsync(string email);
+    Task<User?> GetByIdAsync(Guid id);
+    Task<User> CreateAsync(User user);
+}
